950baa1173145b45955ecbf1a31ed3cf382ff0e7
u3206769 <alex.catherine@live.com>
2021-09-03 17:31:30 +1000

    saves

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -1,11 +1,13 @@
 <footer>
             <p> Logged in as: <?php echo $_SESSION["username"]; ?></p>
-            <ul class="nav">
-            <li><a onClick="return confirm('Do you really want to leave?');" href="logout.php">Log Out</a></li>
-            <li><a href="reset.php">Password Reset</a></li>
+            <nav>
+            <ul>
+                <li><a onClick="return confirm('Do you really want to leave?');" href="logout.php">Log Out</a></li>
+                <li><a href="reset.php">Password Reset</a></li>
             </ul>
+            </nav>
 </footer>
 
 </body>
 
-</html
+</html>
--- public/welcome.php
+++ public/welcome.php
@@ -23,8 +23,5 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     </div>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
+    
     <?php include "templates/footer.php"; ?>
